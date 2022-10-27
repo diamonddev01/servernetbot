fe36d855a029085340115878f1abb85de0fcb2dd
Oreo <thediamondgamer331@gmail.com>
2022-10-27 23:10:31 +0100

    ModalSubmitInteraction support

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, Interaction, InteractionType } from "discord.js";
+import { CommandInteraction, Interaction, InteractionType, ModalSubmitInteraction } from "discord.js";
 import { Client } from "../classes/Client";
 import { Command } from "../classes/Command";
 
@@ -6,13 +6,26 @@ export function event(client: Client, interaction: Interaction): void {
     if (!interaction.guildId) return;
 
     if (interaction.type == InteractionType.ApplicationCommand) CommandHandle(client, <CommandInteraction>interaction);
+    if (interaction.type == InteractionType.ModalSubmit) modalHandler(client, <ModalSubmitInteraction>interaction);
 }
 
-function CommandHandle(client: Client, interaction: CommandInteraction) {
+function CommandHandle(client: Client, interaction: CommandInteraction): void {
     const cmdName = interaction.commandName;
     const cmd = client.commands.get(cmdName);
 
     if (cmd) {
         cmd.evt_command(client, interaction);
+    }
+}
+
+function modalHandler(client: Client, interaction: ModalSubmitInteraction): void {
+    let cmdName: string | string[] = interaction.customId.split(':');
+    if (!cmdName[1]) return;
+    cmdName = cmdName[0];
+
+    const cmd = client.commands.get(cmdName);
+
+    if (cmd) {
+        cmd.evt_interaction(client, interaction);
     }
 }
