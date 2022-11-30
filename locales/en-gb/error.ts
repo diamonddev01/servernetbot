f3de43e6cfbf47e61ca8882329a65970181e544d
Diamond <thediamondgamer331@outlook.com>
2022-11-30 01:20:52 +0000

    Clearup

--- locales/en-gb/error.ts
+++
@@ -1,2 +0,0 @@
-export const NO_PERMISSIONS = "You dont have permission to do that";
-export const ERROR = "Error";
--- src/network/sendMessage.ts
+++
@@ -1,6 +0,0 @@
-import { Message } from "discord.js";
-import { Client } from "../classes/Client";
-
-export async function sendNetworkMessage(client: Client, message: Message) {
-    //
-}
